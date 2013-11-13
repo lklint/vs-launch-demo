29d8fd2b43e35e905f5f6be799f6ba2d9a9bd984
lklint <lars@klint.co>
2013-11-13 17:08:47 +1100

    Initial Commit

---
+++ VSLaunchDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VSLaunchDemo.Startup))]
+namespace VSLaunchDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
